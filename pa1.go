4c07dbc4cd926c9c0216f9796738e06e250198b6
Isaias Perez Vega <32isaias@gmail.com>
2018-07-13 21:52:02 -0400

    reading processes

--- pa1.go
+++ pa1.go
@@ -12,9 +12,11 @@ import (
   "fmt"
   //"io"
   //"io/ioutil"
+  "strings"
   "strconv"
   "os"
 )
+
 
 // --------------------------- //
 // Streamlining error checking //
@@ -32,13 +34,14 @@ func setupInfo(words []string) (processCount, runFor, quantum, algorithm int) {
   fmt.Println("Runing setup:\n") // debug
 
   // Searching, converting, and assigning setup info
-  processCount = toInt("processcount", words)
-  runFor = toInt("runfor", words)
-  quantum = toInt("quantum", words)
+  processCount = lookConvert("processcount", words)
+  runFor = lookConvert("runfor", words)
+  quantum = lookConvert("quantum", words)
   algorithm = algoType(words)
 
   return processCount, runFor, quantum, algorithm
 }
+
 
 // ----------------------------------------- //
 // Represent the type of algorithm as an int //
@@ -56,8 +59,22 @@ func algoType(words []string) (int) {
   return algo
 }
 
-// ------------------------------- //
-// Search location of word in list //
+
+// ------------------------- //
+// Convert string to integer //
+func toInteger(word string) (int) {
+  num, err := strconv.Atoi(word)
+
+  // Type conversion error handling
+  if err != nil {
+    fmt.Println("Conversion of var ", word, " failed!\n")
+  }
+  return num
+}
+
+
+// ---------------------------- //
+// Search index of word in list //
 func find(item string, list []string) (index int) {
   count := 0
   index = -1
@@ -72,13 +89,13 @@ func find(item string, list []string) (index int) {
   if index == -1 {
     fmt.Println("Did not find ", item, " in memory..")
   }
-
   return index
 }
 
-// --------------------- //
-// Convert string to int //
-func toInt(word string, words []string) (int) {
+
+// ------------------------------------------------ //
+// Looks for a word in array and converts it to int //
+func lookConvert(word string, words []string) (int) {
   index := find(word, words)
   num, err := strconv.Atoi(words[index + 1])
 
@@ -90,9 +107,77 @@ func toInt(word string, words []string) (int) {
   return num
 }
 
+
+// ------------------------------------------- //
+// Struct holds information about each process //
+type process struct {
+  ID string
+  arrival int
+  burst int
+}
+
+
+// ----------------------------------------------- //
+// Get processes information needed for scheduling //
+func qProcesses(algorithm int)([]process) {
+  // Reading file
+  file, err := os.Open("c10-rr.in")
+  checkErr(err)
+  defer file.Close()
+
+  // Scanner buffer
+  scanner := bufio.NewScanner(file)
+  scanner.Split(bufio.ScanLines)
+
+  // Storage
+  var lines []string
+  var procs []process
+
+  //fmt.Println("About to read line by line:\n")
+
+  counter := 0
+  procLine := 0
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+    if algorithm == 3 {
+      
+    }
+    fmt.Println(lines[counter], "\n")
+    counter = counter + 1
+  }
+  fmt.Println("lINES[4]: ", lines[4]) //
+  procLine = procLine
+  return procs
+
+}
+
+// ----------------------------------------------- //
+// Returns a process struct from string            //
+// No need to search since input is structured as: //
+// "process name ID arrival # burst #"             //
+func extractInfo(line string) (process) {
+
+  // Allocating resources
+  var proc process
+  var words []string
+
+  // Splitting string
+  words = strings.Split(line, " ")
+
+  // Defining struct
+  proc = process{ID: words[2], arrival: toInteger(words[4]), burst: toInteger(words[6])}
+
+  for i := 0; i < len(words); i = i + 1 {
+    fmt.Println("Index", i, ": ", words[i], "\n")
+  }
+  return proc
+}
+
+
 // -------------------------------- //
 // Execute FCFS Scheduling Algotihm //
 func runFCFS()() {
+
 
 }
 
@@ -112,7 +197,7 @@ func runRR()() {
 func main () {
 
   // Reading input file
-  input, err := os.Open("c10-rr.in") //c10-rr.in
+  input, err := os.Open("c2-fcfs.in") //c10-rr.in
   checkErr(err)
   defer input.Close()
 
@@ -148,14 +233,38 @@ func main () {
   }
 
 
-
-
+  // DEBUGGING //
   processCount = processCount
   runFor = runFor
   quantum = quantum
   algorithm = algorithm
 
   fmt.Println("pcount: ", processCount, " runfor: ", runFor, " quantum: ", quantum, "algorithm: ", algorithm)
+
+  var p = "hello this is not your average hello world"
+  fmt.Println("About to print: ", p)
+  str := strings.Split(p, " ")
+
+  // Testing splitting text and accessing words
+  fmt.Println("Splitting string:\n", strings.Split(p, " "))
+  fmt.Println("Printing by index: \n", "Index 0: ", str[0], "\nIndex 6: ", str[5])
+  if strings.Contains(p, "world") {
+    fmt.Println("World was found at index: ", strings.Index(p, "world"))
+  }
+
+  ss := qProcesses(algorithm)
+  ss = ss
+
+  // Testing struct info
+  var newproc process
+  newproc = extractInfo("process name P2 arrival 0 burst 9")
+  newproc = newproc
+  fmt.Println("Reading process struct:\n")
+  fmt.Println("ID: ", newproc.ID, " arrival: ", newproc.arrival, " burst: ", newproc.burst)
+
+  // find use
+    // check if rr
+  // extract processes
 
 
 
