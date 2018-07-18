122df0cdfc53f7276be118c82821b6031ef1ac17
Isaias Perez Vega <32isaias@gmail.com>
2018-07-17 23:55:42 -0400

    updates

--- pa1.go
+++ pa1.go
@@ -1,5 +1,4 @@
 // Created By Isaias Perez Vega
-// NID: is352549
 // CPU - Scheduling Algorithms
 // Program implements the first-come firs-served, shortest-job first,
 // and round robin algorithms.
@@ -67,7 +66,7 @@ func toInteger(word string) (int) {
 
   // Type conversion error handling
   if err != nil {
-    fmt.Println("Conversion of var ", word, " failed!\n")
+    //fmt.Println("Conversion of var ", word, " failed!\n")
   }
   return num
 }
@@ -116,6 +115,7 @@ type process struct {
   burst int
   wait int
   turnaround int
+  run int
 }
 
 
@@ -307,7 +307,7 @@ func runFCFS(proccessNum, runFor, quantum int, procs []process)() {
 
 // ------------------------------- //
 // Execute SJF Scheduling Algotihm //
-func runSJF(proccessNum, runFor, quantum int, procs []process)() {
+func runSJFbk(proccessNum, runFor, quantum int, procs []process)() {
 
 
   time := 0                    // Clock
@@ -391,17 +391,479 @@ func runSJF(proccessNum, runFor, quantum int, procs []process)() {
   }
 }
 
+// ------------------------------- //
+// Execute SJF Scheduling Algotihm //
+func runSJF(proccessNum, runFor, quantum int, procs []process)() {
+
+
+  time := 0                    // Clock
+  run := true                  // Flag terminates algorithm
+  busy := false                // True indicates a process is running
+  finish := 0                  // Tracks the process tthat was selected
+  began := 0                   // Tracks when in time the process was selected
+  proc := 0                    // Tracks the index of the current process
+  //inHold := false              // Flag indicates when a process preemptived
+  partialProcSize := 0         //
+  minBurstIndex := 0           //
+  var holdProc, minProc process
+  burst := 0
+  currentProc := new(process)
+  //currIndex := 0
+
+  fmt.Println(" ", proccessNum, " processes")
+  fmt.Println("Using preemptive Shortest Job First")
+  for run {
+
+    // process arrived
+    for i := 0; i < len(procs); i = i + 1 {
+      if time == procs[i].arrival {
+        fmt.Println("Time ", time, " : ", procs[i].ID, " arrived")
+
+        // Find min burst of all arrivals
+        for j := 0; j <= i; j = j + 1 {
+          if procs[j].burst < procs[minBurstIndex].burst {
+            minBurstIndex = j
+          }
+        }
+
+        break
+      }
+    }
+
+    holdProc = holdProc //
+    partialProcSize = partialProcSize //
+    minProc = minProc //
+    burst = burst
+
+    // Current process is always the min of arrivals
+    // currentProc.ID = procs[minBurstIndex].ID
+    // currentProc.arrival = procs[minBurstIndex].arrival
+    // currentProc.burst = procs[minBurstIndex].burst
+    //fmt.Println("current: ", currentProc.ID)
+
+
+
+    // Process finished
+    if currentProc.burst == 0 && busy {
+      fmt.Println("Time ", time, " : ", currentProc.ID, " finished")
+
+      //procs[minBurstIndex] = *currentProc
+      //procs[minBurstIndex].ID = currentProc.ID
+      //procs[minBurstIndex].arrival = currentProc.arrival
+      //procs[minBurstIndex].burst = currentProc.burst
+
+      if proc < len(procs) {
+        proc = proc + 1
+      }
+      busy = false
+    }
+
+    // Selecting process to run Next
+    if !busy && proc < len(procs) {
+      if procs[minBurstIndex].burst > 0 {
+        fmt.Println("Time ", time, " : ", currentProc.ID, " selected (burst ", currentProc.burst,")")
+        busy = true
+
+      }
+      //fmt.Println("Time ", time, " : ", procs[minBurstIndex].ID, " selected (burst ", procs[minBurstIndex].burst,")")
+
+    }
+
+    // Running selected process
+    if currentProc.burst > 0 {
+      currentProc.burst = currentProc.burst - 1
+      fmt.Println("Time ", time, " : ", currentProc.ID, " arrival: ", currentProc.arrival, "burst: ", currentProc.burst)
+    }
+
+
+    //Selecting process to run next
+    // if !busy && proc < len(procs){
+    //   if time >= procs[proc].arrival {
+    //     //fmt.Println("Time ", time, " : ", procs[proc].ID, " selected (burst ", procs[proc].burst,")")
+    //     busy = true
+    //     finish = proc
+    //     began = time
+    //     currentProc = procs[proc]
+    //     //selecTime = append(selecTime, time)
+    //   }
+    // }
+
+
+    // process idles state
+
+
+    time = time + 1
+
+    if time == runFor {
+      fmt.Println("Finished at time ", time, "\n")
+      run = false
+    }
+    began, finish = began, finish
+  }
+}
+
+func remove(slice []*process, s int) []*process {
+    return append(slice[:s], slice[s+1:]...)
+}
+
+
+// ----------------------------------------- //
+// Create a process instance and fill fields //
+func create(pnum, arrival, burst, wt, ta, run int, ID string )(*process) {
+  var node *process
+  node = new(process)
+
+  // node.ID = proc.ID
+  // node.arrival = proc.arrival
+  // node.burst = proc.burst
+  // node.wait = proc.wait
+  // node.turnaround = proc.turnaround
+  // node.run = proc.run
+  node.pnum = pnum
+  node.ID = ID
+  node.arrival = arrival
+  node.burst = burst
+  node.wait = wt
+  node.turnaround = ta
+  node.run = run
+
+  return node
+}
+
+// --------------------------------------- //
+// Execute Round Robin Scheduling Algotihm //
+func runRRbk(proccessNum, runFor, quantum int, procs []process)() {
+  run := true                     // Initiates/Terminates algorithm
+  busy := false                   // Inidicates when a process is running
+  time := 0                       // Clock
+  proc := 0                       // Number of process completed
+  ranFor := 0                     // Tracks runtime of a process
+  //var requestQuee []process       // Circular queue
+  var currProc *process            // Current process at work
+  currProc = new(process)
+  var requestQuee = make([]process, len(procs))
+  size := 0
+
+
+  passedby := 0
+
+  // Clock
+  for run {
+
+    // Process Arrival
+    for i := 0; i < len(procs); i = i + 1 {
+      if time == procs[i].arrival {
+        fmt.Println("Time ", time, " : ", procs[i].ID, " arrived")
+
+        //requestQuee = append(requestQuee, procs[i])
+        requestQuee[size] = procs[i]
+         //curr process cant be set everytime a proc arrives!
+        size = size + 1
+
+        for i := 0; i < len(requestQuee); i = i + 1 { //
+          //fmt.Println("reqQ[", i, "]: ", requestQuee[i].ID, "burst: ", requestQuee[i].burst)
+        }
+
+        //fmt.Println("appended: ", requestQuee[size].ID, "s: ", size, "curr: ", currProc.ID)
+        //fmt.Println("curr: ", currProc.ID, " run: ", currProc.run, " burst: ", currProc.burst)
+        //fmt.Println("Added by arrival: ", procs[i].ID, "rqSize: ", len(requestQuee)) //db
+        break
+      }
+    }
+
+    *currProc = requestQuee[size - 1]
+
+    // Get the first element of queue
+    // currProc.ID = requestQuee[0].ID
+    // currProc.arrival = requestQuee[0].arrival
+    // currProc.burst = requestQuee[0].burst
+    // currProc.run = requestQuee[0].run
+    //fmt.Println("curr: ", currProc.ID, " run: ", currProc.run, " burst: ", currProc.burst)
+
+    //*currProc = requestQuee[0]
+    //fmt.Println("curr: ", currProc.ID, " run: ", currProc.run)
+    //currProc.run = currProc.run + 1
+    //fmt.Println("curr: ", currProc.ID, " run: ", currProc.run)
+    //currProc.run = time
+    //fmt.Println("curr: ", currProc.ID, " run: ", currProc.run)
+
+
+    //fmt.Println("about to hit finish")
+    //fmt.Println("t: ", time," curr: ", currProc.ID, " run: ", currProc.run) //db
+
+    // Check if Process Finished
+    if busy && (currProc.run == currProc.burst) && (currProc.run != 0) {
+      fmt.Println("Time ", time, " : ", currProc.ID, " finished: ", currProc.burst - currProc.run)
+
+      // Remove it from request queue
+      if size > 0 {
+        size = size - 1
+      }
+      //remove(requestQuee,  0)
+
+      // Processor availabe
+      busy = false
+
+      // Tracking number of completed processes
+      if proc < len(procs) {
+        proc = proc + 1
+      }
+    }
+
+    // Process Selected
+    if  proc < len(procs) {
+
+         // Processor is free
+         if !busy {
+
+           // Select next process in queue
+           *currProc = requestQuee[0];
+           fmt.Println("Time ", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")")
+           busy = true
+         }
+
+
+         // Time Quantum
+         // Processor can be free or working
+         if time % quantum == 0 {
+
+           // Getting next process in queue
+           *currProc = requestQuee[0]
+
+           // If we get here, it means that currProc has not finished
+           // save progress and add to queue
+           requestQuee[size] = *currProc
+           size = size + 1
+
+           // Checking next process is a valid process to run
+           if requestQuee[0].arrival <= time {
+
+             // select it as the new process to run
+             *currProc = requestQuee[0]
+             fmt.Println("Time ", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")")
+           }
+
+
+           // Check if it has run for completion
+           // If so it was removed from quee already just select new one
+           if currProc.run == currProc.burst && currProc.run != 0 {
+             //if size < len(procs) {//
+
+             //}
+             //remove(requestQuee, size) //
+             //currProc = &requestQuee[0]
+             // currProc.ID = requestQuee[0].ID
+             // currProc.arrival = requestQuee[0].arrival
+             // currProc.burst = requestQuee[0].burst
+             // currProc.run = requestQuee[0].run
+             //fmt.Println("Time s", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")")
+
+           // If not save progress and re-add to quee
+           } else {
+             //remove(requestQuee, 0)
+             //requestQuee = append(requestQuee, *currProc) //
+
+           }
+
+    }
+
+
+      //fmt.Println("quantum time: ", time, "currProc: ", currProc.ID)
+    }
+
+
+
+    // Process Idle
+
+
+
+
+    currProc = currProc
+
+
+
+    // Clock and runtime updates
+    time = time + 1
+    ranFor = ranFor + 1
+    currProc.run = currProc.run + 1
+    passedby = passedby + 1 //
+    //fmt.Println(passedby, "currRun: ", currProc.run, " from: ", currProc.ID)
+
+    // Terminate Algorithm
+    if time == runFor {
+      fmt.Println("Finished at time ", time, "\n")
+      run = false
+    }
+
+  }
+}
+
 // --------------------------------------- //
 // Execute Round Robin Scheduling Algotihm //
 func runRR(proccessNum, runFor, quantum int, procs []process)() {
+  run := true                     // Initiates/Terminates algorithm
+  busy := false                   // Inidicates when a process is running
+  time := 0                       // Clock
+  proc := 0                       // Number of process completed
+  ranFor := 0                     // Tracks runtime of a process
+  var requestQuee []*process       // Circular queue
+  var currProc *process            // Current process at work
 
+
+  // Clock
+  for run {
+
+    // Process Arrival
+    for i := 0; i < len(procs); i = i + 1 {
+      if time == procs[i].arrival {
+        fmt.Println("Time ", time, " : ", procs[i].ID, " arrived")
+
+
+        // currProc = create(requestQuee[0].pnum, requestQuee[0].arrival,
+        // requestQuee[0].burst, requestQuee[0].wait, requestQuee[0].turnaround,
+        // ranFor, requestQuee[0].ID)
+
+        requestQuee = append(requestQuee, &procs[i]) //
+        //requestQuee = append(requestQueem, currProc)
+
+        //requestQuee[size] = procs[i] //
+        //size = size + 1//
+
+        for i := 0; i < len(requestQuee); i = i + 1 { //
+          //fmt.Println("reqQ[", i, "]: ", requestQuee[i].ID, "burst: ", requestQuee[i].burst)
+        }
+
+        break
+      }
+    }
+
+    // currProc = create(requestQuee[0].pnum, requestQuee[0].arrival,
+    // requestQuee[0].burst, requestQuee[0].wait, requestQuee[0].turnaround,
+    // ranFor, requestQuee[0].ID)
+
+    //currProc = create(requestQuee[0])
+
+    // Check if Process Finished
+    if  (currProc.run == currProc.burst) {
+      //fmt.Println("Time ", time, " : ", currProc.ID, " finished: ", currProc.burst - currProc.run)
+
+      // Dequeue first process
+      requestQuee = requestQuee[1:]
+
+      // Create instance of new process
+      currProc = create(requestQuee[0].pnum, requestQuee[0].arrival,
+      requestQuee[0].burst, requestQuee[0].wait, requestQuee[0].turnaround,
+      requestQuee[0].run, requestQuee[0].ID)
+
+      // Processor availabe
+      busy = false
+      ranFor = currProc.run
+
+      // Tracking number of completed processes
+      if proc < len(procs) {
+        proc = proc + 1
+      }
+    }
+
+    // Select Process
+    if  proc < len(procs) {
+
+      // Time Quantum
+      // Processor can be free or working
+      if time % quantum == 0 {
+
+        // If we get here, it means that currProc has not finished
+        // save progress and re-add to queue
+        if currProc.run != currProc.burst {
+          currProc = create(currProc.pnum, currProc.arrival,
+          currProc.burst, currProc.wait, currProc.turnaround,
+          ranFor + 1, currProc.ID)
+
+          // Enqueue process
+          requestQuee = append(requestQuee, currProc)
+
+          // Dequeue process
+          requestQuee = requestQuee[1:]
+
+          // Update currProc
+          currProc = create(requestQuee[0].pnum, requestQuee[0].arrival,
+          requestQuee[0].burst, requestQuee[0].wait, requestQuee[0].turnaround,
+          requestQuee[0].run, requestQuee[0].ID)
+
+
+
+        }
+        //fmt.Fprintf("this is)%3d")
+        // Processor is free
+        if  currProc.arrival <= time {
+
+          // Select next process in queue
+          //*currProc = requestQuee[0];
+          //currProc = create(requestQuee[0])
+          //fmt.Println("Time ", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")")
+          //fmt.Println("Time ", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")", "run: ", currProc.run)
+          busy = true
+        }
+
+
+        for i:=0;i < len(requestQuee); i = i + 1 {
+          //fmt.Println("i: ", i, " ",requestQuee[i].ID, "run: ", requestQuee[i].run )
+        }
+
+
+        //requestQuee = append(requestQuee, currProc)
+
+        //requestQuee = requestQuee[1:] //
+
+        // // Checking next process is a valid process to run
+        // if requestQuee[0].arrival <= time {
+        //
+        //   // select it as the new process to run
+        //   //currProc = create(requestQuee[0])
+        //   currProc = create(requestQuee[0].pnum, requestQuee[0].arrival,
+        //   requestQuee[0].burst, requestQuee[0].wait, requestQuee[0].turnaround,
+        //   requestQuee[0].run, requestQuee[0].ID)
+        //   fmt.Println("selencted from quantum")
+        //   fmt.Println("Time ", time, " : ", currProc.ID, " selected (burst ", currProc.burst,")")
+        //   busy = true
+        // }
+      }
+
+
+    }
+
+
+
+    // Process Idle
+
+
+    // Runtime Updates
+    time = time + 1
+    ranFor = ranFor + 1
+    fmt.Println("Ran: ", currProc.run, "ranFor: ",ranFor, " ID: ", currProc.ID)
+    currProc = create(currProc.pnum, currProc.arrival,
+    currProc.burst, currProc.wait, currProc.turnaround,
+    ranFor, currProc.ID)
+    fmt.Println("curr.run: ", currProc.run)
+    currProc = currProc
+    busy = busy
+
+    // Terminate Algorithm
+    if time == runFor {
+      fmt.Println("Finished at time ", time, "\n")
+      run = false
+    }
+
+  }
 }
+
 
 
 func main () {
 
   // Read file and close it when done
-  file := "c10-sjf.in"
+  file := "c5-rr.in"
+  //file := os.Args[1]
   input, err := os.Open(file)
   checkErr(err)
   defer input.Close()
@@ -437,11 +899,7 @@ func main () {
 
 
   // DEBUGGING //
-  processCount = processCount
-  runFor = runFor
-  quantum = quantum
-  algorithm = algorithm
-
+  processCount, runFor, quantum, algorithm = processCount, runFor, quantum, algorithm
   //fmt.Println("pcount: ", processCount, " runfor: ", runFor, " quantum: ", quantum, "algorithm: ", algorithm, "\n")
 
 
